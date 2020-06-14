e657fd136de421f3a11f0baa97991307887a6714
zeeshan saleem <zeeshansaleem9291@gmail.com>
2020-06-14 17:13:39 +0500

    ready assignmnt

---
+++ app.js
@@ -0,0 +1,360 @@
+// task 1
+
+// alert("Welcome to my website");
+// alert("Error! Please enter a valid password");
+// alert("Wellcome to JS land..." + "\r\n" + "Happy coding")
+// prompt("Happy coding","Prevent this page from creating additional dialogues")
+// console.log("Hello... I can run my js through web browser's console..")
+
+
+
+// task 2
+// var username="";
+// var Myname="Zeeshan Saleem"
+// var message="Hello World";
+// alert(message);
+// // students bio data
+
+// var name="zeeshan";
+// var age="15years old";
+// var cert="certified mobile application";
+// alert(name)
+// alert(age)
+// alert(cert)
+
+// var a = "pizza";
+// alert(a[0]+a[1]+a[2]+a[3]+a[4]+ "\r\n" +a[0]+a[1]+a[2]+a[3]+ "\r\n" +a[0]+a[1]+a[2]+ "\r\n" +a[0]+a[1]+ "\r\n" +a[0])
+
+
+// var email="zeeshansaleem9291@gmail.com";
+// alert("My email address is" +" " + email)
+
+
+
+// var book="A smarter way to learn JavaScript";
+// alert(book)
+
+
+// document.write("Yah! i can write html content through java script"+"<br>")
+
+// var b="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(b)
+// document.write(b)
+
+
+// task 3
+// var age="23"
+// alert("I am " + age + "years old")
+
+// var j=0;
+// for(j = 0; j>=0; j++){
+//     alert("You have visited this site " +j + "times ")
+//     break
+// }
+
+// var birthyear=1997;
+// document.write("My birth year is " +birthyear +"<br>")
+// document.write("data type my declared variable is number"+"<br>")
+
+
+// var visitor_name = "John Doe";
+// var Product_title ="T-Shirts";
+// var quantity =5;
+// document.write(visitor_name +"Ordered" + quantity +Product_title +"on xyz clothing store")
+
+
+
+
+// task 4
+
+// var a,b,c;
+// // // legal
+// var d;
+// var e;
+// var f;
+// var g;
+// var h;
+// // illegal
+// var 1g;
+// var 2a;
+// var )e;
+// var hs sds;
+// var sdsd.ds;
+// document.write("Rules for naming variables in js" +"<br>")
+
+// document.write("Variable names can only contain numbers, $ and __ For example $my_1stVariable"+ "<br>")
+// document.write("Variables must begin with a letter , $ or _. For example $name, _name or name" + "<br>")
+// document.write("Variable names are case sensitive"+ "<br>")
+// document.write("Variable names should not be JS keyword")
+
+
+// task 5
+//  Addition
+// var a=+prompt("Enter first no")
+// var b=+prompt("Enter second  no")
+// var c = a+b
+// document.write("sum of " + a +" and " +b+" is " + c)
+
+// subtraction
+// var a1=+prompt("Enter first no")
+// var b1=+prompt("Enter second  no")
+// var c1= a1-b1
+// document.write("sub of " + a1 +" and " +b1+" is " + c1)
+
+// multiplication
+// var a2=+prompt("Enter first no")
+// var b2=+prompt("Enter second  no")
+// var c2 = a2+b2
+// document.write("Mul of " + a2 +" and " +b2+" is " + c2)
+
+// division
+// var a3=+prompt("Enter first no")
+// var b3=+prompt("Enter second  no")
+// var c3 = a3+b3
+// document.write("division of " + a3 +" and " +b3+" is " + c3)
+
+// modulus
+// var a4=+prompt("Enter first no")
+// var b4=+prompt("Enter second  no")
+// var c4 = a4+b4
+// document.write("sum of " + a4 +" and " +b4+" is " + c4)
+
+
+// multiplication table
+// for (var i=1; i<=10; i++){
+//      document.write("4"+"*" + i +"=" + i*4 +"<br>");
+//  }
+
+// task 6-9
+
+// var a=+prompt("Enter a no")
+// document.write("Value of a no is "+ a +"<br>")
+// document.write("The value of ++a is " + ++a + "<br>")
+// document.write("Now the value of a is " + a + "<br>")
+// document.write("Now the  value of a is " + ++a +"<br>")
+// document.write("The value of --a is " + --a + "<br>")
+// document.write("Now the value of a is " + a + "<br>")
+// document.write("Now the value of a-- is " + a-- + "<br>")
+// document.write("Now the value of a is " + a + "<br>")
+
+
+
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--
+
+
+// --a; 1
+// --a - --b; 0 - 0
+// --a - --b + ++b; 2-2
+// --a - --b + ++b + b--; 3-2
+//  document.write(result)
+
+
+
+
+
+// var a =prompt("Enter your name:")
+// alert(a+ "..! welcome to my website")
+
+
+
+// var a=prompt("Enter any no:")
+// if(a ==undefined){
+    
+    
+//     for (var i=1; i<=10; i++){
+//          document.write( 5+ "*" + i +"=" + i*i+"<br>");
+// }
+
+// }
+// else
+//     var a
+//     for (var i=1; i<=10; i++){
+//            document.write(a + "*" + i +"=" + i*a +"<br>");
+//         }
+
+
+
+// task 9-11
+
+// var city =prompt("Enter your City:")
+// if (city=="Karachi" || city== "karachi"){
+//     alert("Welcome to city of lights")
+// }
+
+
+
+// var gender=prompt("Enter your gender:")
+// if (gender=="Female" ||gender=="female"){
+//     alert(" Good Morning Ma’am.")
+// }
+// if (gender=="male" ||gender=="Male"){
+//     alert(" Good Morning Sir.")
+// }
+
+
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+
+
+
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+
+
+//     if("car" < "cat"){
+//         alert("car is smaller than cat");
+//         }    
+
+
+// task 12-13
+// var a = +prompt("enter first no")
+// var b = +prompt("enter second no")
+// if (a>b){
+//     alert(a + " Is larger then " + b)
+// }
+// if (b>a){
+//     alert(b + " Is larger then " + a)
+// }
+// if(a===b){
+//     alert("both are equal")
+// }
+
+
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// alert(greeting)
+// }
+// else
+// {
+//     greeting = "Good evening";
+//     alert(greeting)
+// }
+
+// task 14-16
+
+
+// sudents=[];
+// a=[1,2,3,4,5]
+// b=["a1","a2","a3"]
+// c = ["True","False"]
+// d = [1,2,"a","c","abcd"]
+
+// ed=["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil.", "PhD"]
+// document.write( ed [0] + "<br>")
+// document.write( ed [1] + "<br>")
+// document.write( ed [2] + "<br>")
+// document.write( ed [3] + "<br>")
+// document.write( ed [4] + "<br>")
+// document.write( ed [5] + "<br>")
+// document.write( ed [6] + "<br>")
+// document.write( ed [7] + "<br>")
+
+
+// task 17-20
+// arr=[[0,1,2,3],[1,0,1,2],[2,1,,0,1]]
+// for (var i =0; i<arr.length ; i++ ){
+//     document.write(arr[i] + "<br>")
+// }
+
+
+
+// for(var i=0; i<=10; i++){
+//     document.write(i + "<br>")
+// }
+
+// Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+
+// var a = +prompt("Enter any no: ")
+// var b = +prompt("Enter any length: ")
+// for (var i=0; i<=b; i++ ){
+//     document.write(a + "*" + i + "=" + a*i + "<br>")
+// }
+
+
+
+// Write a program to print items of the following array
+// using for loop:
+// fruits = ["apple", "banana", "mango", "orange","strawberry"]
+// for (var i =0; i<fruits.length ; i++ ){
+//     document.write(fruits[i] + "<br>")
+//     document.write("Element at index" + i + "is" + fruits[i] + "<br>")
+// }
+
+
+// Write a program to identify the largest number in the
+// given array.
+// A = [24, 53, 78, 91, 12].max()
+// document.write(A)
+
+
+// Write a program to identify the smallest number in the
+// given array.
+// A = [24, 53, 78, 91, 12].min()
+// document.write(A)
+
+
+// Write a program to print multiples of 5 ranging 1 to
+// 100.
+
+
+
+
+
+
+// arr=[[0,1,2,3],[1,0,1,2],[2,1,,0,1]]
+// for (var i =0; i<arr.length ; i++ ){
+//     document.write(arr[i] + "<br>")
+// }
+
+
+
